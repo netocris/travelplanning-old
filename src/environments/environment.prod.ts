c4884139975f5b533096ab38a3654b55d0fed3a2
Cristóvão Neto <netocris@gmail.com>
2019-09-21 23:27:56 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  contextPath: '/travelplanning',
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
 };
