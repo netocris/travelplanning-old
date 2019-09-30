1ca432f550a3def4404036401e8a37fa3e2ce0d2
Cristóvão Neto <netocris@gmail.com>
2019-09-30 23:12:51 +0100

    dark mode

--- src/app/components/organisms/footer/footer.component.ts
+++ src/app/components/organisms/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { BaseComponent } from '../../base/base.component';
 
 @Component({
@@ -10,12 +11,30 @@ export class FooterComponent extends BaseComponent {
   
   year: number = 1800;
 
-  constructor() {
+  constructor(@Inject(DOCUMENT) private document: Document) {
     super();
   }
 
   protected ngOnInitCustom(): void {
     this.year = new Date().getFullYear();
+  }
+
+  onChange(value: any): void {
+    console.log(value.target.checked);
+    if(value.target.checked){
+      this.trans();
+      this.document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      this.trans();
+      this.document.documentElement.setAttribute('data-theme', 'light');
+    }
+  }
+
+  private trans(): void {
+    this.document.documentElement.classList.add('transition');
+      window.setTimeout(() => {
+        this.document.documentElement.classList.remove('transition');
+      }, 1000);
   }
 
 }
