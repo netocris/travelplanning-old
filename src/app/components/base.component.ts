fb78c9796bf5b76f36c6cd1c5a434c7474ffc512
Cristóvão Neto <netocris@gmail.com>
2019-10-09 23:16:23 +0100

    refact

--- src/app/components/base.component.ts
+++ src/app/components/base.component.ts
@@ -1,12 +1,10 @@
 import { OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
 
 export abstract class BaseComponent implements OnInit {
 
   stillLoading: boolean = false;
   protected timeout: number = 100;
-  protected subscribtion: Subscription = null;
-
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/components/organisms/list-record/list-record.component.ts
+++ src/app/components/organisms/list-record/list-record.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Record } from '../../../models/record';
 import { BaseComponent } from '../../base.component';
 import { RecordService } from '../../../services/record.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-record',
@@ -10,6 +11,8 @@ import { RecordService } from '../../../services/record.service';
 })
 export class ListRecordComponent extends BaseComponent {
 
+  private recordsSubscription: Subscription = null;
+  
   records: Record[] = [];
 
   constructor(private recordService: RecordService) {
@@ -17,20 +20,18 @@ export class ListRecordComponent extends BaseComponent {
   }
 
   protected ngOnInitCustom(): void {    
-    this.stillLoading = true;
-    setTimeout(() => {
-      this.subscribtion = this.recordService.getRecords().subscribe((data: Record[]) => {
-        if (data) {
-          this.records = data;
-          this.stillLoading = false;
-        }
-      });
-    }, this.timeout); 
+    this.stillLoading = true;    
+    this.recordsSubscription = this.recordService.getRecords().subscribe((data: Record[]) => {
+      if (data) {
+        this.records = data;
+        this.stillLoading = false;
+      }
+    });    
   }
 
   protected ngOnDestroyCustom(): void {
-    if (this.isEmptyObject(this.subscribtion)) {
-      this.subscribtion.unsubscribe();
+    if (this.isEmptyObject(this.recordsSubscription)) {
+      this.recordsSubscription.unsubscribe();
     }
   }
   
