99c4c97d95f97aab07b95eeeb49ce0eb9cfb97fe
Cristóvão Neto <netocris@gmail.com>
2019-10-09 23:11:45 +0100

    add and list views

--- src/app/components/organisms/add-record/add-record.component.ts
+++ src/app/components/organisms/add-record/add-record.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BaseComponent } from '../../base.component';
+import { RecordService } from '../../../services/record.service';
+import { Record } from '../../../models/record';
 
 @Component({
   selector: 'app-add-record',
   templateUrl: './add-record.component.html',
   styleUrls: ['./add-record.component.css']
 })
-export class AddRecordComponent implements OnInit {
+export class AddRecordComponent extends BaseComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private recordService: RecordService) {
+    super();
   }
 
+  save(data: Record): void {
+    this.recordService.save(data);
+  }
+  
 }
--- src/app/components/organisms/edit-record/edit-record.component.ts
+++ src/app/components/organisms/edit-record/edit-record.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BaseComponent } from '../../base.component';
 
 @Component({
   selector: 'app-edit-record',
   templateUrl: './edit-record.component.html',
   styleUrls: ['./edit-record.component.css']
 })
-export class EditRecordComponent implements OnInit {
+export class EditRecordComponent extends BaseComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    super();
   }
+
+  
 
 }
