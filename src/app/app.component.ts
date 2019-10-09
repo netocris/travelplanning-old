ad8113b8d06f12c5c4a27215bbd0b2b2d38890fd
Cristóvão Neto <netocris@gmail.com>
2019-10-09 21:46:14 +0100

    i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ConfigService } from './services/config.service';
-import { LocaleEnum } from './types/locale.enum';
+import { LocaleEnum } from './enum/locale.enum';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +9,12 @@ import { LocaleEnum } from './types/locale.enum';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'travelplanning';
 
-  constructor(private translateService: TranslateService, configService: ConfigService){
-    /* this language will be used as a fallback when a translation isn't found in the current language */
-    translateService.setDefaultLang(configService.getStringKey(LocaleEnum.LOCALE));
-    /* this language to use, if the language isn't available, it will use the current loader to get them */
-    translateService.use(configService.getStringKey(LocaleEnum.LOCALE));
+  constructor(private translateService: TranslateService, private configService: ConfigService){    
+    this.translateService.setDefaultLang(this.configService.getStringKey(LocaleEnum.LOCALE));    
+    this.translateService.use(this.configService.getStringKey(LocaleEnum.LOCALE));
   }
   
 }
